3ab96da0d3858b249330d3801305dd85fa90cb93
Dhriti <sdhriti0711@gmail.com>
2024-04-11 16:37:12 +0530

    till lecture 3

--- App.js
+++ App.js
@@ -1,16 +1,4 @@
-const parent= React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"It is a h1 tag"),React.createElement("h2",{},"It is a h2 tag")]))
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 
-
-
-// const parent= React.createElement("div",{id:"parent"},
-// React.createElement("div",{id:"child"},
-// React.createElement("h1",{},"It is nested div")))
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-// const heading= React.createElement("h1",{id:"heading"},"Hello World React By Dhriti");
-//         const root = ReactDOM.createRoot(document.getElementById("root"));
-//         root.render(heading);
